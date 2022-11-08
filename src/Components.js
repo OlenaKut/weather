4da416ab861e8c22f05f9e5502f26245cb3a43e7
OlenaKut <cundrochka@gmail.com>
2022-11-08 18:33:28 +0100

    4222

--- src/Components.js
+++ src/Components.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Card } from "semantic-ui-react";
 import moment from "moment";
 import "./App.css";
-import "semantic-ui-css/semantic.min.css";
+
 
 const refresh = () => {
   window.location.reload();
