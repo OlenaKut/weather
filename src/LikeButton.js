f02f75610633c55df9d9c7195931ad65d4a1f24a
OlenaKut <cundrochka@gmail.com>
2022-11-28 20:42:51 +0100

    -logs?

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -3,18 +3,17 @@ import { Button } from "semantic-ui-react";
 
 function LikeButton() {
   const [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
 
   useEffect(() => {
-    setCalculation(() => count + 1);
-  }, [count]);
+    setCount((count) => count + 1);
+  }, []);
 
   return (
     <>
       {" "}
       <Button
         onClick={() => setCount((c) => c + 1)}
-        content={calculation - 1}
+        content={count - 2}
         icon={{ color: "red", name: "like" }}
       ></Button>
     </>
